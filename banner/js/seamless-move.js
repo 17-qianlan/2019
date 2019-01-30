68d6ff37e64ade3131037c8847b6f3c928e123c6
qianlan <qlhdtly12189@Gmail.com>
2019-01-30 12:00:49 +0800

    three

--- banner/js/seamless-move.js
+++ banner/js/seamless-move.js
@@ -1,43 +1,74 @@
 (function(){
-    const aBanner = document.querySelector('#banner .banner-list .banner-img');
-    const aImg = aBanner.querySelectorAll('li');
-    const aP = document.querySelectorAll("#banner .banner-list .dir p");
-    const aBtn = document.querySelectorAll('#banner .banner-list .banner-btn .wrap-btn span');
+    const oBanner = document.querySelector('#banner');
+    const oBanner_list = oBanner.querySelector('.banner-list .banner-img');
+    const aImg = oBanner_list.querySelectorAll('li');
+    const oDir = oBanner.querySelector(' .banner-list .dir')
+    const aP = oDir.querySelectorAll('p');
+    const aBtn = oBanner.querySelectorAll('.banner-list .banner-btn .wrap-btn span');
     let index = 0;
     const len = aImg.length - 1;
     let date = new Date();
+    let timer = null;
+    function banner(fn, isAdd){
+        aBtn[index].classList.remove('active');
+        fn();
+        if (isAdd) {
+            animate(oBanner_list, 'left', -index*800, 1000, function(){
+                if (len === index){
+                    index = 0;
+                    oBanner_list.style.left = 0 + 'px';
+                }
+            });
+        } else {
+            animate(oBanner_list, 'left', -index*800, 1000);
+        }
+        aBtn[index === len? 0: index].classList.add('active');
+    }
     for (let i=0; i<aP.length; i++) {
         aP[i].onclick = function() {
             if (new Date() - date > 1200){
                 if (i === 0) {
-                    aBtn[index].classList.remove('active');
-                    index = index <= 0? len-1: --index;
-                    animate(aBanner, 'left', -index*800, 1000);
-                    aBtn[index].classList.add('active');
-                } else {
-                    aBtn[index].classList.remove('active');
-                    index = index >= len? 0: ++index;
-                    animate(aBanner, 'left', -index*800, 1000, function(){
-                        if (len === index){
-                            index = 0;
-                            aBanner.style.left = 0 + 'px';
+                    banner(function(){
+                        index = index <= 0? len-1: --index;
+                        if (index === len-1) {
+                            oBanner_list.style.left = -len*800 + 'px';
                         }
-                    });
-                    aBtn[index === len? 0: index].classList.add('active');
+                    })
+                } else {
+                    banner(function(){
+                        index = index >= len? 0: ++index;
+                    }, true);
                 }
                 date = new Date();
             }
         }
     }
-    for( let i=0; i<aBtn.length; i++) {
-        if (new Date() - date > 1200){
-            aBtn[i].onclick = function(){
-                aBtn[index].classList.toggle('active');
-                index = i;
-                animate(aBanner, 'left', -index*800, 1000);
-                aBtn[index].classList.toggle('active');
+    for(let i=0; i<aBtn.length; i++) {
+        aBtn[i].onclick = function(){
+            if (new Date() - date > 1200){
+                banner(function(){
+                    index = i;
+                })
+                date = new Date();
             }
-            date = new Date();
         }
+
     }
+    timer = setInterval(function(){
+        banner(function(){
+            index = index >= len? 0: ++index;
+        }, true);
+    }, 2000);
+    oBanner.onmouseenter = function(){
+        clearInterval(timer);
+        animate(oDir, 'opacity', 1, 1000);
+    };
+    oBanner.onmouseleave = function(){
+        animate(oDir, 'opacity', 0, 1000);
+        timer = setInterval(function(){
+            banner(function(){
+                index = index >= len? 0: ++index;
+            }, true);
+        }, 2000);
+    };
 })();
